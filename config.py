ffc4e1e596084df365c7d15a0424174f67b1221d
Komarov Serhii <s-e-r-g-e-y@ukr.net>
2015-06-06 20:12:56 +0300

    configs

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'somethingimpossibletoguessxd'
+
+RECORDS_PER_PAGE = 10
