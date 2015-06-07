c20859306ebef9690ff78080acdf87a289ece999
Komarov Serhii <s-e-r-g-e-y@ukr.net>
2015-06-07 19:05:09 +0300

    heroku

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'somethingimpossibletoguessxd'
